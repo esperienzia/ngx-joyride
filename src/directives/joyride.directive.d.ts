1ec1ee7080231fc43501804334bbffcb990747ce
jari tarabelli <j.tarabelli@nsi.it>
2019-02-11 17:37:54 +0100

    dist update i forgot the startsHidden

--- src/directives/joyride.directive.d.ts
+++ src/directives/joyride.directive.d.ts
@@ -23,6 +23,7 @@ export declare class JoyrideDirective implements AfterViewInit {
     nextTemplate?: TemplateRef<any>;
     doneTemplate?: TemplateRef<any>;
     counterTemplate?: TemplateRef<any>;
+    startsHidden?: boolean;
     delayEmitter?: EventEmitter<any>;
     prev?: EventEmitter<any>;
     next?: EventEmitter<any>;
